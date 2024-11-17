50c2a33844a700a072c491ef08552cdacec0996c
Parth <Srivastav.Parth@gmail.com>
2024-11-16 19:51:04 -0800

    Add Bootstrap and placeholder NavBar

--- react-self/src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-function CustomHeader() {
-  return (
-    <>
-      <p>New Header goes here</p>
-    </>
-  );
-}
-export default CustomHeader;
--- react-self/src/components/navbar.tsx
+++ react-self/src/components/navbar.tsx
@@ -1,13 +1,23 @@
-function NavBar() {
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function NavBarWrapper() {
   return (
     <div>
-      <ul>
-        <li>About me</li>
-        <li>LoL</li>
-        <li>Anime</li>
-      </ul>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Parth Srivastav</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#lol">Lol</Nav.Link>
+              <Nav.Link href="#anime">Anime</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
 
-export default NavBar;
+export default NavBarWrapper;
